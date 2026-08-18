45c50cbac0cec1e88a8dfef538cab715480dbc9f
Muhammad Ayaz Hameed <ayazhameed7500@gmail.com>
2026-08-18 17:16:54 +0500

    Remove duplicate worknest.softcenterci folder

--- worknext/worknest.softcenterci/src/Component/Subscriptions/SubscriptionSidebar.jsx
+++
@@ -1,9 +0,0 @@
-import SubscriptionCards from "./subscriptionCards.jsx"
-import SubscriptionTable from "./SubscriptionTable.jsx"
-export default function Subscriptionsidebar(){
-    return(<>
-    <SubscriptionCards />
-    <SubscriptionTable />
-    
-    </>)
-}
--- worknext/worknest.softcenterci/src/pages/Chat.jsx
+++
@@ -1,9 +0,0 @@
-import ChatLayout from "../components/chat/ChatLayout";
-
-export default function Chat() {
-  return (
-    <div className="h-[calc(100vh-90px)] overflow-hidden rounded-2xl bg-white shadow-sm">
-      <ChatLayout />
-    </div>
-  );
-}
